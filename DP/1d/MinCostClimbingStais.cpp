50bb8881b6e1c9bcef8abc621f311ad49363f7e8
avin-1 <avinashbhurke8@gmail.com>
2026-03-20 11:52:24 +0530

    minimum cost climbing stairs

---
+++ DP/1d/MinCostClimbingStais.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+using namespace std;
+
+
+/*
+dp[i] = minimum cost to reach step i
+dp[0] = cost[0]
+dp[1] = cost[1]
+dp[i] = cost[i] + min(dp[i-1], dp[i-2])
+*/
+int minicost(vector<int>& cost) {
+    int n = cost.size();
+    vector<int> dp(n);
+    dp[0] = cost[0];
+    dp[1] = cost[1];
+    for (int i = 2; i < n; i++)
+        dp[i] = cost[i] + min(dp[i-1], dp[i-2]);
+    return min(dp[n-1], dp[n-2]);
+}
+int main(){
+    vector<int> cost = {10,15,20};
+    cout<<minicost(cost);
+    return 0;
+}
