c47a12ab161b3b8f0a51a0d0e7c31d2a9dabc736
avin-1 <avinashbhurke8@gmail.com>
2026-04-10 14:13:14 +0530

    add note

--- DP/1d/MinCostClimbingStais.cpp
+++ DP/1d/MinCostClimbingStais.cpp
@@ -3,13 +3,14 @@
 #include <climits>
 using namespace std;
 
-
+// A brute force solution would be to check every climb(n-1) and climb(n-2) for every stair  to reduce it we just cache it using dp
 /*
 dp[i] = minimum cost to reach step i
 dp[0] = cost[0]
 dp[1] = cost[1]
 dp[i] = cost[i] + min(dp[i-1], dp[i-2])
 */
+
 int minicost(vector<int>& cost) {
     int n = cost.size();
     vector<int> dp(n);
